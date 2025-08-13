9c45ba2f4b21f56a4f44ea94da676bf30232be7e
MonsieurNaexec <naexec@m-naexec.be>
2025-08-13 21:45:02 +0200

    fix build

--- database/factories/book_category_factory.ts
+++
@@ -1,8 +0,0 @@
-import factory from '@adonisjs/lucid/factories'
-import BookCategory from '#models/book_category'
-
-export const BookCategoryFactory = factory
-  .define(BookCategory, async ({ faker }) => {
-    return {}
-  })
-  .build()
