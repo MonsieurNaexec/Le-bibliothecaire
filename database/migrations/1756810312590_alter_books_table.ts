203d27d4911cdb27a1175141af6eb766869b6a01
Naexec <naexec@onvep.be>
2025-09-02 12:58:02 +0200

    increase max url length

---
+++ database/migrations/1756810312590_alter_books_table.ts
@@ -0,0 +1,17 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'books'
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('url', 2048).alter()
+    })
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('url', 255).alter()
+    })
+  }
+}
